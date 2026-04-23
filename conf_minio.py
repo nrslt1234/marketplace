30398b64039c4448705454a7de5c68b8c644923a
Nursultan <ss3t9n@gmail.com>
2026-04-23 21:01:33 +0300

    1

--- conf_minio.py
+++ conf_minio.py
@@ -3,7 +3,7 @@ from minio import Minio
 
 
 client = Minio(
-    "s3.kamaldinovnt.ru:9000",          # без http://
+    "s3.kamaldinovnt.ru",          # без http://
     access_key=os.environ["MINIO_ROOT_USER"],
     secret_key=os.environ["MINIO_ROOT_PASSWORD"],
     secure=True,                       # HTTPS
